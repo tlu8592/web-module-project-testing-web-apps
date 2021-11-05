c2f8508a6aab0454cc838c8f68254090c1fbfecf
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-11-05 00:10:43 -0400

    test passed: renders ONE error message if user enters less than 5 characters into firstname

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -17,8 +17,14 @@ test('renders the contact form header', ()=> {
     expect(header).toBeInTheDocument();
 });
 
-test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+test('renders ONE error message if user enters less than 5 characters into firstname.', async () => {
+    render(<ContactForm />)
+
+    const firstNameInput = screen.queryByPlaceholderText('Edd');
+    userEvent.type(firstNameInput, 'Edd');
+
+    const firstNameError = screen.queryByTestId("error");
+    expect(firstNameError).toBeInTheDocument();
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
