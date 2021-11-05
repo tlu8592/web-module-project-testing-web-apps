e3b83aa9188a987ebc9d439cf1a7b3c9ebb9b443
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-11-04 23:47:31 -0400

    test passed: renders the contact form header

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -10,7 +10,11 @@ test('renders without errors', ()=>{
 });
 
 test('renders the contact form header', ()=> {
-    
+    render(<ContactForm />)
+
+    const header = screen.getByText(/contact form/i)
+
+    expect(header).toBeInTheDocument();
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
